388ea0173e458d259ba731a2eede5849bb5cc8e4
Caipeng <icaipeng@gmail.com>
2020-08-23 19:21:58 +0800

    [Modified] 均摊复杂度，手写动态数组

---
+++ 00-Vector/MyVector.h
@@ -0,0 +1,60 @@
+
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class MyVector {
+
+private:
+    T *data;
+    int capacity;
+    int size;
+
+    // O(n)
+    void resize(int newCapacity) {
+        assert(newCapacity >= size);
+        T *newData = new T[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        delete[] data;
+        data = newData;
+        capacity = newCapacity;
+    }
+
+public:
+
+    MyVector() {
+        data = new T[10];
+        capacity = 10;
+        size = 0;
+    }
+
+    ~MyVector() {
+        delete[] data;
+    }
+
+    // O(1) 均摊复杂度
+    void push_back(T e) {
+        // assert(size<capacity);
+        if (size == capacity) {
+            resize(2 * capacity);
+
+        }
+
+        data[size++] = e;
+    }
+
+    // O（1）
+    void pop_back() {
+        assert(size > 0);
+        T ret = data[size - 1];
+        size--;
+        if (size == capacity / 4) {
+            resize(capacity / 2);
+        }
+        return ret;
+    }
+
+};
---
+++ 00-Vector/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include "MyVector.h"
+
+using namespace std;
+int main() {
+    for (int i = 10; i < 20; i++)
+    {
+        int n = pow(2,i);
+
+        clock_t  start_time = clock();
+
+         MyVector<int> vec;
+         for (int i = 0; i < n; i++)
+         {
+             vec.push_back(i);
+         }
+        clock_t  end_time = clock();
+        cout<<2*n<<" operation: \t";
+        cout<<double(end_time-start_time)/CLOCKS_PER_SEC<<" s"<<endl;
+
+    }
+    
+    return 0;
+}

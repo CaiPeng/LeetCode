2a5a3e9149eaaf60e2160a29222454730d733dc7
Caipeng <icaipeng@gmail.com>
2020-08-23 19:22:50 +0800

    [Modified] code format

--- 00-Vector/main.cpp
+++ 00-Vector/main.cpp
@@ -4,23 +4,22 @@
 #include "MyVector.h"
 
 using namespace std;
+
 int main() {
-    for (int i = 10; i < 20; i++)
-    {
-        int n = pow(2,i);
+    for (int i = 10; i < 20; i++) {
+        int n = pow(2, i);
 
-        clock_t  start_time = clock();
+        clock_t start_time = clock();
 
-         MyVector<int> vec;
-         for (int i = 0; i < n; i++)
-         {
-             vec.push_back(i);
-         }
-        clock_t  end_time = clock();
-        cout<<2*n<<" operation: \t";
-        cout<<double(end_time-start_time)/CLOCKS_PER_SEC<<" s"<<endl;
+        MyVector<int> vec;
+        for (int i = 0; i < n; i++) {
+            vec.push_back(i);
+        }
+        clock_t end_time = clock();
+        cout << 2 * n << " operation: \t";
+        cout << double(end_time - start_time) / CLOCKS_PER_SEC << " s" << endl;
 
     }
-    
+
     return 0;
 }
